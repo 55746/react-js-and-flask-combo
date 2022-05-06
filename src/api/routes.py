436ce02741c0673bb9d2121e0e93d20e5c2c4649
55746 <Lawhead.gavin@live.jobcorps.org>
2022-05-06 19:28:16 +0000

    commit2

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,4 +1,8 @@
 """
+
+# PIPENV RUN START
+# NPM RUN START
+
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
@@ -6,7 +10,7 @@ from api.models import db, User
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
-
+# ITS API.ROUTE BECAUSE OF THIS LINE HERE
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
@@ -16,3 +20,24 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/signup', methods=['GET'])
+def get_user():
+    user=User.query.all()
+    user_list=list(map(lambda x: x.serialize(), user))
+    return jsonify(user_list), 200
+
+
+    # HAD TO CREATE A POST FOR THE SIGN UP
+
+@api.route('/signup', methods=['POST'])
+def create_user():
+    response_body= request.get_json()
+    if 'email' not in response_body:
+        raise APIException('bad request, email needed', status_code=400)
+    if 'password' not in response_body:
+        raise APIException('bad request, password needed', status_code=400)
+    new_user=User(email=response_body['email'], password=response_body['password'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(new_user.serialize()), 200
